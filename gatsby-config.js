2cb94977a212277529be37a1c21e880d760b1a42
Patricia Silva <patricia.silva@ab-inbev.com>
2020-06-20 22:36:35 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `gss-patricia personal sie`,
+    description: `Patricia's personal site.`,
+    author: `@gss-patricia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
